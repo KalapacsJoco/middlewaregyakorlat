db0f4c7f1d8120cac6f1972468c52f6e872e947b
KalapacsJoco <kalapacs.joco79@gmail.com>
2024-10-10 16:30:43 +0300

    37percig fasza

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -1,15 +1,22 @@
+import { useContext } from "react";
 import {Link, Outlet} from "react-router-dom"
+import { AppContext } from "./Context/AppContext";
 
 export default function Layout() {
+    const {user} = useContext(AppContext)
     return (
         <>
         <header>
             <nav>
                 <Link to="/" className="nav-link">Home</Link>
-                <div>
+                {user ? (<div>{user.name}</div>
+                ) :(
+                    <div>
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
-                </div>
+                </div>        
+                )}
+
             </nav>
         </header>
 
